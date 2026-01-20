e3334474a4970acc80fb0a330501d500f6a86afe
9elt <nelt.primary@gmail.com>
2026-01-20 16:18:11 +0100

    chore: format

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 export var jsx = (key, props) => typeof key === "function"
-    ? (key = key(props), key instanceof Promise && (key.$ = props.$), key)
-    : createNode((props.tagName = key, delete props.$, props));
+    ? (
+        key = key(props),
+        key instanceof Promise && (key.$loading = props.$loading),
+        key
+    )
+    : createNode((
+        props.tagName = key,
+        delete props.$loading,
+        props
+    ));
+
 export var Fragment = (props) => props.children;
+
 export var createNode = (props) =>
     typeof props === "string" || typeof props === "number"
         ? window.document.createTextNode(props)
@@ -10,8 +20,10 @@ export var createNode = (props) =>
             : props instanceof window.Node
                 ? props
                 : props instanceof Promise
-                    ? (props.then((p) => props.replaceWith(createNode(p))),
-                        props = createNode(props.$))
+                    ? (
+                        props.then((p) => props.replaceWith(createNode(p))),
+                        props = createNode(props.$loading)
+                    )
                     : Array.isArray(props)
                         ? createNode({ tagName: "div", children: props })
                         : assign(
@@ -21,17 +33,24 @@ export var createNode = (props) =>
                             ),
                             props
                         );
-var assign = (on, from) => (Object.keys(from).forEach((prop) =>
-    prop === "tagName" || prop === "namespaceURI" ? 0
-        : prop === "children"
-            ? append(on, from[prop])
-            : typeof from[prop] === "object"
-                ? assign(on[prop], from[prop])
-                : (from[prop] || from[prop] === 0) &&
-                (on.namespaceURI === "http://www.w3.org/2000/svg"
-                    ? on.setAttribute(prop, from[prop])
-                    : on[prop] = from[prop])
-), on);
+
+var assign = (on, from) => (
+    Object.keys(from).forEach((prop) =>
+        prop === "tagName" || prop === "namespaceURI"
+            ? 0
+            : prop === "children"
+                ? append(on, from[prop])
+                : typeof from[prop] === "object"
+                    ? assign(on[prop], from[prop])
+                    : (from[prop] || from[prop] === 0) && (
+                        on.namespaceURI === "http://www.w3.org/2000/svg"
+                            ? on.setAttribute(prop, from[prop])
+                            : on[prop] = from[prop]
+                    )
+    ),
+    on
+);
+
 var append = (on, children) => Array.isArray(children)
     ? children.forEach((child) => append(on, child))
     : on.appendChild(createNode(children));
